d3e51b86fcae75a9dbf401b6a93400f1555b450c
Renan Barbosa <renan.barbosa@netbiis.local>
2023-04-05 15:43:56 -0300

    First commit

---
+++ dslearn-bootcamp/backend/src/main/java/com/renanbarhbosa/dslearnbootcamp/DslearnBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.renanbarhbosa.dslearnbootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslearnBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslearnBootcampApplication.class, args);
+	}
+
+}
---
+++ dslearn-bootcamp/backend/src/test/java/com/renanbarhbosa/dslearnbootcamp/DslearnBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renanbarhbosa.dslearnbootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
